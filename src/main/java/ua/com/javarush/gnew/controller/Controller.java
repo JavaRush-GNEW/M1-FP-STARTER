1eaecd83e3dfe0cc39ff7d8149fe48de4e3dbb7f
DevEvge <evgeniivolkov18@gmail.com>
2024-09-03 01:01:54 +0300

    Add Disabled fields

--- src/main/java/ua/com/javarush/gnew/controller/Controller.java
+++ src/main/java/ua/com/javarush/gnew/controller/Controller.java
@@ -33,7 +33,6 @@ public class Controller implements Initializable {
     private FileManager fileManager = new FileManager();
     private Cypher cypher = new Cypher();
 
-
     private String selectedPath;
 
     @FXML
@@ -66,11 +65,23 @@ public class Controller implements Initializable {
     @FXML
     void choeseButtonCliced(ActionEvent event) {
         File selectedFile = fileChooser.showOpenDialog(new Stage());
-        actualPathLabel.setText(selectedFile.toString());
+        if (selectedFile != null) {
+            actualPathLabel.setText(selectedFile.toString());
+            if (dropdown.getValue().equals("Encrypt") || dropdown.getValue().equals("Decrypt")) {
+                inputKey.setDisable(false);
+            } else if (dropdown.getValue().equals("Bruteforce")) {
+                doCypherButton.setDisable(false);
+            }
+            openNewFileDir.setDisable(true);
+        }
     }
 
     @FXML
     void keyInputSelected(KeyEvent event) {
+        // Разблокировка кнопки DoCypher, если inputKey в фокусе и нажата клавиша
+        if (inputKey.isFocused()) {
+            doCypherButton.setDisable(false);
+        }
     }
 
     @FXML
@@ -101,7 +112,7 @@ public class Controller implements Initializable {
 
             } else if (dropdown.getValue().equals("Bruteforce")) {
                 Command command = Command.BRUTEFORCE;
-                Integer key = Integer.parseInt(inputKey.getText());
+                Integer key = 0;
                 Path filePath = Path.of(actualPathLabel.getText());
                 selectedPath = actualPathLabel.getText();
                 RunOptions runOptions = new RunOptions(command, key, filePath);
@@ -110,17 +121,26 @@ public class Controller implements Initializable {
                 Path newFilePath = changeFileName.newFileNameWithKey(runOptions, "DECRYPTED", result.getKey());
                 fileManager.write(newFilePath, result.getDecryptedContent());
             }
+
+            // Блокируем элементы после выполнения шифрования/дешифрования
+            if (!dropdown.getValue().equals("Bruteforce")) {
+                inputKey.setDisable(true);
+            }
+            doCypherButton.setDisable(true);
+            openNewFileDir.setDisable(false);
+
+            // Очистка поля inputKey
+            inputKey.clear();
         } else {
             status.setText("Поля не заполнены");
         }
         actualPathLabel.setText("");
         status.setText("Done");
-
     }
 
     @FXML
     void methodChoesed(ActionEvent event) {
-//        Выключение отображение поля "Key" если выбран брутфорс
+        choesePathButton.setDisable(false); // Разблокировка кнопки ChoesePath при выборе метода
         if (dropdown.getSelectionModel().getSelectedItem().equals("Bruteforce")) {
             inputKey.setVisible(false);
         } else {
@@ -154,3 +174,179 @@ public class Controller implements Initializable {
         fileChooser.setInitialDirectory(new File("C:\\Users\\evgen\\Desktop"));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package ua.com.javarush.gnew.controller;
+//
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.fxml.Initializable;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.ComboBox;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TextField;
+//import javafx.scene.input.KeyEvent;
+//import javafx.scene.input.MouseEvent;
+//import javafx.scene.layout.AnchorPane;
+//import javafx.stage.FileChooser;
+//import javafx.stage.Stage;
+//import ua.com.javarush.gnew.crypto.BruteForceResult;
+//import ua.com.javarush.gnew.crypto.Cypher;
+//import ua.com.javarush.gnew.file.ChangeFileName;
+//import ua.com.javarush.gnew.file.FileManager;
+//import ua.com.javarush.gnew.runner.Command;
+//import ua.com.javarush.gnew.runner.RunOptions;
+//
+//import java.awt.*;
+//import java.io.File;
+//import java.io.IOException;
+//import java.net.URL;
+//import java.nio.file.Path;
+//import java.util.ResourceBundle;
+//
+//public class Controller implements Initializable {
+//
+//    private FileChooser fileChooser = new FileChooser();
+//    private ChangeFileName changeFileName = new ChangeFileName();
+//    private FileManager fileManager = new FileManager();
+//    private Cypher cypher = new Cypher();
+//
+//
+//    private String selectedPath;
+//
+//    @FXML
+//    private Label actualPathLabel;
+//
+//    @FXML
+//    private AnchorPane pane;
+//
+//    @FXML
+//    private ComboBox<String> dropdown;
+//
+//    @FXML
+//    private Button openNewFileDir;
+//
+//    @FXML
+//    private Button choesePathButton;
+//
+//    @FXML
+//    private TextField inputKey;
+//
+//    @FXML
+//    private Button doCypherButton;
+//
+//    @FXML
+//    private Label newFilePath;
+//
+//    @FXML
+//    private Label status;
+//
+//    @FXML
+//    void choeseButtonCliced(ActionEvent event) {
+//        File selectedFile = fileChooser.showOpenDialog(new Stage());
+//        actualPathLabel.setText(selectedFile.toString());
+//    }
+//
+//    @FXML
+//    void keyInputSelected(KeyEvent event) {
+//    }
+//
+//    @FXML
+//    void doCypher(ActionEvent event) {
+//        boolean actualPathEmpty = actualPathLabel.getText().isEmpty();
+//        boolean dropdownEmpty = dropdown.getSelectionModel().isEmpty();
+//
+//        if (!actualPathEmpty && !dropdownEmpty) {
+//            if (dropdown.getValue().equals("Encrypt")) {
+//                Command command = Command.ENCRYPT;
+//                Integer key = Integer.parseInt(inputKey.getText());
+//                Path filePath = Path.of(actualPathLabel.getText());
+//                selectedPath = actualPathLabel.getText();
+//                RunOptions runOptions = new RunOptions(command, key, filePath);
+//                String content = fileManager.read(runOptions.getFilePath());
+//                Path newFilePath = changeFileName.newFileName(runOptions, "ENCRYPTED");
+//                fileManager.write(newFilePath, cypher.encrypt(content, runOptions.getKey()));
+//
+//            } else if (dropdown.getValue().equals("Decrypt")) {
+//                Command command = Command.DECRYPT;
+//                Integer key = Integer.parseInt(inputKey.getText());
+//                Path filePath = Path.of(actualPathLabel.getText());
+//                selectedPath = actualPathLabel.getText();
+//                RunOptions runOptions = new RunOptions(command, key, filePath);
+//                String content = fileManager.read(runOptions.getFilePath());
+//                Path newFilePath = changeFileName.newFileName(runOptions, "DECRYPTED");
+//                fileManager.write(newFilePath, cypher.decrypt(content, runOptions.getKey()));
+//
+//            } else if (dropdown.getValue().equals("Bruteforce")) {
+//                Command command = Command.BRUTEFORCE;
+//                Integer key = Integer.parseInt(inputKey.getText());
+//                Path filePath = Path.of(actualPathLabel.getText());
+//                selectedPath = actualPathLabel.getText();
+//                RunOptions runOptions = new RunOptions(command, key, filePath);
+//                String content = fileManager.read(runOptions.getFilePath());
+//                BruteForceResult result = cypher.bruteforce(content);
+//                Path newFilePath = changeFileName.newFileNameWithKey(runOptions, "DECRYPTED", result.getKey());
+//                fileManager.write(newFilePath, result.getDecryptedContent());
+//            }
+//        } else {
+//            status.setText("Поля не заполнены");
+//        }
+//        actualPathLabel.setText("");
+//        status.setText("Done");
+//    }
+//
+//    @FXML
+//    void methodChoesed(ActionEvent event) {
+////        Выключение отображение поля "Key" если выбран брутфорс
+//        if (dropdown.getSelectionModel().getSelectedItem().equals("Bruteforce")) {
+//            inputKey.setVisible(false);
+//        } else {
+//            inputKey.setVisible(true);
+//        }
+//    }
+//
+//    @FXML
+//    void newFileOpenPath(ActionEvent event) {
+//        File file = new File(selectedPath);
+//        try {
+//            Desktop.getDesktop().open(file.getParentFile());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    @FXML
+//    void paneMouseClicked(MouseEvent event) {
+//        if (inputKey.isFocused()) {
+//            pane.requestFocus();
+//        }
+//    }
+//
+//    @Override
+//    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        String[] items = {"Encrypt", "Decrypt", "Bruteforce"};
+//        dropdown.getItems().addAll(items);
+//
+//        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Текстовые файлы", "*.txt"));
+//        fileChooser.setInitialDirectory(new File("C:\\Users\\evgen\\Desktop"));
+//    }
+//}
