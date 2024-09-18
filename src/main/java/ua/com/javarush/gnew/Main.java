ee053c655ee6b683786177e596650671e647c46a
BevzyukIvan <vanozero050@gmail.com>
2024-09-18 21:07:46 +0300

    Project 1.0v

--- src/main/java/ua/com/javarush/gnew/Main.java
+++ src/main/java/ua/com/javarush/gnew/Main.java
@@ -4,17 +4,15 @@ package ua.com.javarush.gnew;
 import ua.com.javarush.gnew.JavaFX.JavaFXApp;
 import ua.com.javarush.gnew.runner.CipherApplication;
 
+import java.util.Arrays;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            JavaFXApp.launchApp(args);
-        } else {
+
             CipherApplication app = CipherApplication.getInstance();
             app.run(args);
-            app.runInteractive();
-        }
+
     }
 }
