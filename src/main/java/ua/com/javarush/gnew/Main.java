aa58aa16b3f8c3dd70c3ba9c344ae2d097e6ea07
BevzyukIvan <vanozero050@gmail.com>
2024-09-18 22:04:08 +0300

    Commit

--- src/main/java/ua/com/javarush/gnew/Main.java
+++ src/main/java/ua/com/javarush/gnew/Main.java
@@ -1,18 +1,18 @@
 package ua.com.javarush.gnew;
 
 
-import ua.com.javarush.gnew.JavaFX.JavaFXApp;
 import ua.com.javarush.gnew.runner.CipherApplication;
-
-import java.util.Arrays;
 
 
 public class Main {
 
     public static void main(String[] args) {
-
-            CipherApplication app = CipherApplication.getInstance();
-            app.run(args);
-
+//        if (args.length == 0) {
+//            JavaFXApp.launchApp(args);//Графічний інтерфес
+//        } else {
+        CipherApplication app = CipherApplication.getInstance();
+        app.run(args);
+//            app.runInteractive();//Відповідає за консоль
+//        }
     }
 }
