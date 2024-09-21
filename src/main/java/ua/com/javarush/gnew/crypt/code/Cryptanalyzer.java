7aef6b3f8d9e277a1425e78d780de5f73f35df19
Denys <denisburduzan@gmail.com>
2024-09-21 16:05:19 +0300

    simplified my code

--- src/main/java/ua/com/javarush/gnew/crypt/code/Cryptanalyzer.java
+++ src/main/java/ua/com/javarush/gnew/crypt/code/Cryptanalyzer.java
@@ -62,55 +62,8 @@ public class Cryptanalyzer {
     }
 
     public String decryption(String str, int key) {
-        ArrayList<Character> ENG_LOWER_MOD = new ArrayList<>(ENG_LOWER);
-        ArrayList<Character> UKR_LOWER_MOD = new ArrayList<>(UKR_LOWER);
-        Collections.rotate(ENG_LOWER_MOD, key);
-        Collections.rotate(UKR_LOWER_MOD, key);
-
-        ArrayList<Character> ENG_UPPER_MOD = new ArrayList<>(ENG_UPPER);
-        ArrayList<Character> UKR_UPPER_MOD = new ArrayList<>(UKR_UPPER);
-        Collections.rotate(ENG_UPPER_MOD, key);
-        Collections.rotate(UKR_UPPER_MOD, key);
-
-        char[] array = str.toCharArray();
-        StringBuilder builder = new StringBuilder();
-
-        for (char symbol : array) {
-            if (Character.isLowerCase(symbol)) {
-                int originalIndex = ENG_LOWER.indexOf(symbol);
-                if (originalIndex != -1) {
-                    Character decrypted = ENG_LOWER_MOD.get(originalIndex);
-                    builder.append(decrypted);
-                    continue;
-                }
-
-                originalIndex = UKR_LOWER.indexOf(symbol);
-                if (originalIndex != -1) {
-                    Character decrypted = UKR_LOWER_MOD.get(originalIndex);
-                    builder.append(decrypted);
-                } else {
-                    builder.append(symbol);
-                }
-            }
-            else if (Character.isUpperCase(symbol)) {
-                int originalIndex = ENG_UPPER.indexOf(symbol);
-                if (originalIndex != -1) {
-                    Character decrypted = ENG_UPPER_MOD.get(originalIndex);
-                    builder.append(decrypted);
-                    continue;
-                }
-
-                originalIndex = UKR_UPPER.indexOf(symbol);
-                if (originalIndex != -1) {
-                    Character decrypted = UKR_UPPER_MOD.get(originalIndex);
-                    builder.append(decrypted);
-                } else {
-                    builder.append(symbol);
-                }
-            } else {
-                builder.append(symbol);
-            }
-        }
+        StringBuilder builder;
+        builder = new StringBuilder(encryption(str, -key));
         return builder.toString();
     }
 }
